a953af5a992e2f34c51ad6aca91a158facf096d2
unknown <rrmail@ukr.net>
2022-09-27 19:29:21 +0300

    upd

--- src/js/gallery-render.js
+++ src/js/gallery-render.js
@@ -32,7 +32,8 @@ export default class GalleryRender {
       downloads,
     } = item;
 
-    return `<div class="photo-card" >
+    return `
+    <div class="photo-card" >
            <a class="gallery__item"
              href="${largeImageURL}">
              <img
@@ -41,19 +42,24 @@ export default class GalleryRender {
                loading="lazy" />
            </a>
            <div class="info">
-             <p class="info-item">Likes
-               <p>${likes}</p>
+             <p class="info-item">
+             <b>Likes</b>
+               ${likes}
              </p>
-             <p class="info-item">Views
-               <p>${views}</p>
+             <p class="info-item">
+             <b>Views</b>
+               ${views}
              </p>
-             <p class="info-item">Comments
-               <p>${comments}</p>
+             <p class="info-item">
+             <b>Comments</b>
+               ${comments}
              </p>
-             <p class="info-item">Downloads
-               <p>${downloads}</p>
+             <p class="info-item">
+             <b>Downloads</b>
+               ${downloads}
              </p>
            </div>
-          </div>`;
+          </div>
+          `;
   }
 }
